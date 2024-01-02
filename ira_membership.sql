f5d99beb82949489fed3bf9809630aab68de5e5f
eerawr <74672138+syakirahira@users.noreply.github.com>
2024-01-03 01:13:55 +0800

    Add files via upload

--- ira_membership.sql
+++ ira_membership.sql
@@ -18,8 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
-CREATE DATABASE IF NOT EXISTS ira_membership;
-USE ira_membership;
+-- Database: `ira_membership
 --
 
 -- --------------------------------------------------------
